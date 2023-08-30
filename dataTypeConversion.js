2b6bc8f9620a6eb6999657d5683d2a516814eed0
vee <95954865+veecodes07@users.noreply.github.com>
2023-08-30 10:33:10 +0000

    edit 2

--- dataTypeConversion.js
+++
@@ -1,9 +0,0 @@
-let babdi = 21
-
-let stringBabdi = String(babdi)
-console.log(stringBabdi);
-console.log(typeof stringBabdi);
-
-let booleanBabdi = Boolean(babdi)
-console.log(booleanBabdi);
-console.log(typeof booleanBabdi);
--- num_and_math.js
+++
@@ -1,5 +0,0 @@
-const score = 400;
-
-const balance = new Number(1000);
-console.log(score);
-console.log(balance);
