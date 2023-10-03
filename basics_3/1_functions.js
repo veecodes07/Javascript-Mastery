d1d24ea5ec586e5ef19c45948a49b9d4083e6a2c
vee <95954865+veecodes07@users.noreply.github.com>
2023-10-03 11:47:42 +0000

    func1

--- basics_3/1_functions.js
+++ basics_3/1_functions.js
@@ -6,8 +6,15 @@
 function addTwoNumbers (number1, number2) {
     let result = number1 + number2
     return result
+    //return number1 + number2
  }
 
  const result = addTwoNumbers(3,5)
 
  console.log("result:", result);
+
+ function loginUserMessage(username) {
+   return `${username} has just logged in`
+ }
+
+ console.log(loginUserMessage("vgod"));
