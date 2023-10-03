cd3db69b416f467395d7f11c4b979cdf05a5e8e6
vee <95954865+veecodes07@users.noreply.github.com>
2023-10-03 11:12:26 +0000

    function1

---
+++ basics_3/1_functions.js
@@ -0,0 +1,13 @@
+//function addTwoNumbers (number1, number2) {
+   // console.log(number1 + number2);
+//}
+
+
+function addTwoNumbers (number1, number2) {
+    let result = number1 + number2
+    return result
+ }
+
+ const result = addTwoNumbers(3,5)
+
+ console.log("result:", result);
