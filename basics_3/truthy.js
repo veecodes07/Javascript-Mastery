eb081e030250cea89a01862c24e448fdcbe42c7f
vee <95954865+veecodes07@users.noreply.github.com>
2023-10-18 11:24:32 +0000

    truthy switch

---
+++ basics_3/truthy.js
@@ -0,0 +1,18 @@
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("object is empty");
+}
+
+//CHECK WHETHER THE ASSIGNED VALUES ARE TRUE OR FALSE IN CONST VAL = 100;
+//FALSY VALUES : false, 0, -0, Bignint, 0n, "", null, undefined, NaN
+//TRUTHY VALUES : "0", 'FALSE', " ", [], {}, FUNCTION(){}
+
+// Nullish Coalescing OPerator(??): null undefined
+// let val1 ;
+// val1 = 5 ?? 10
+// val1= null ?? 5
+// val1 = 10 ?? 5
+// val1 = undefined ?? 5
+
+// console.log(" val1");
