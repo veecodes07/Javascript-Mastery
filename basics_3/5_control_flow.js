24dd8a69aa450805321d0dd62bafa579bbbdff7c
vee <95954865+veecodes07@users.noreply.github.com>
2023-10-18 11:11:53 +0000

    control flow

---
+++ basics_3/5_control_flow.js
@@ -0,0 +1,53 @@
+//if statements
+
+// const isUserLoggerIn = true
+// const temperature = 60;
+
+// if (temperature > 50) {
+//     console.log("greater than 50");
+// } else {
+//     console.log("less than 50");
+// }
+
+// console.log("this statement will always execute");
+
+// // <,>, ==, ===. !=, !==, <=, >=
+
+// // never use var keyword while defining const in if else statement cause it has global scope
+
+// //example
+// const score = 200;
+// if(score = 200){
+//     const power = "fly" //if you use var instead of const it will access this variable everywhere in code.
+//     console.log(`user power: ${power}`);
+// }
+
+
+// //EXAMPLE OF IMPLICIT CODE
+//  const balance = 1000;
+
+//  if ( balance> 500) console.log("test"); //single line, assumes there is a scope.
+
+//  //ELSE IF STATEMENNT
+// //  if (balance < 500) {
+// //     console.log("less than 500");
+// //  } else if (balance <750){
+// //     console.log("less than 750");
+// //  } else if (balance < 1200) {
+// //     console.log("less than 1200");
+// //  }
+
+// const userLoggedin = true
+// const debitcard = true
+// const loggedinFromGoogle = false
+// const loggedinFromEmail = true
+
+// if (userLoggedin && debitcard && 2==2) {
+//     console.log("allow user to pay");
+// }
+// if (loggedinFromGoogle || loggedinFromEmail) {
+//     console.log("logged in");
+// }
+
+
+// && FOR AND OPERATOR , || FOR OR OPERATOR
